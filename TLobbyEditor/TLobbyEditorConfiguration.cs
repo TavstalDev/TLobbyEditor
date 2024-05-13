c1ef96264a35c39eacce6bfeb6148eab0ab1eef8
Tavstal <tavstal@gmail.com>
2024-05-13 18:06:13 +0200

    Minor changes

--- TLobbyEditor/TLobbyEditorConfiguration.cs
+++ TLobbyEditor/TLobbyEditorConfiguration.cs
@@ -1,32 +1,56 @@
-﻿using Tavstal.TLibrary.Compatibility;
+﻿using Newtonsoft.Json;
+using Tavstal.TLibrary.Compatibility;
 using Tavstal.TLobbyEditor.Models;
 
 namespace Tavstal.TLobbyEditor
 {
     public class TLobbyEditorConfiguration : ConfigurationBase
     {
+        [JsonProperty(Order = 3)]
         public bool ShouldAdversiteServer { get; set; }
+        [JsonProperty(Order = 4)]
         public bool HideRocket { get; set; }
+        [JsonProperty(Order = 5)]
         public bool HideWorkshop { get; set; }
+        [JsonProperty(Order = 6)]
         public bool MessWorkshop { get; set; }
+        [JsonProperty(Order = 7)]
         public string[] Workshop { get; set; }
+        [JsonProperty(Order = 8)]
         public bool HideConfig { get; set; }
+        [JsonProperty(Order = 9)]
         public bool MessConfig { get; set; }
+        [JsonProperty(Order = 10)]
         public string[] Config { get; set; }
+        [JsonProperty(Order = 11)]
         public bool HidePlugins { get; set; }
+        [JsonProperty(Order = 12)]
         public bool MessPlugins { get; set; }
+        [JsonProperty(Order = 13)]
         public string[] Plugins { get; set; }
+        [JsonProperty(Order = 14)]
         public bool MessGamemode { get; set; }
+        [JsonProperty(Order = 15)]
         public string Gamemode { get; set; }
+        [JsonProperty(Order = 16)]
         public bool IsPVP { get; set; }
+        [JsonProperty(Order = 17)]
         public bool HasCheats { get; set; }
+        [JsonProperty(Order = 18)]
         public string Difficulty { get; set; } // EASY, NORMAL, HARD
+        [JsonProperty(Order = 19)]
         public string CameraMode { get; set; } // FP, TP, BOTH, VEHICLE 
+        [JsonProperty(Order = 20)]
         public bool GoldOnly { get; set; }
+        [JsonProperty(Order = 21)]
         public bool HasBattleye { get; set; }
+        [JsonProperty(Order = 22)]
         public string Description { get; set; }
+        [JsonProperty(Order = 23)]
         public string DescriptionHint { get; set; }
+        [JsonProperty(Order = 24)]
         public string[] DescriptionFull { get; set; }
+        [JsonProperty(Order = 25)]
         public ReservedSlots ReservedSlots { get; set; }
 
         public override void LoadDefaults()
